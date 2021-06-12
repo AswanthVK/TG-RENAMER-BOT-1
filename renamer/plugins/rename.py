4cb0e7ca2f7c385a877e0573e7bd8dd44e5bfc80
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-12 11:21:18 +0530

    Update rename.py

--- renamer/plugins/rename.py
+++ renamer/plugins/rename.py
@@ -46,7 +46,7 @@ async def media(c, m):
         if time_gap:
             return
 
-    file_name = await c.ask(chat_id=m.from_user.id, text=ForceReply(True), filters=filters.text)
+    file_name = await c.ask(chat_id=m.from_user.id, reply_markup=ForceReply(True), filters=filters.reply_markup)
     await file_name.delete()
     await file_name.request.delete()
     new_file_name = file_name.text
