925088cf9cee43d69f4267691a2686be7a08b47c
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-12 12:46:24 +0530

    Update callback.py

--- renamer/plugins/callback.py
+++ renamer/plugins/callback.py
@@ -10,6 +10,14 @@ from pyrogram.errors import UserBannedInChannel, UserNotParticipant
 from pyrogram.emoji import *
 
 
+################## Callback for Rename button ##################
+
+@RenamerNs.on_callback_query(filters.regex('rename_button'))
+async def rename_button_cback(c,m):
+  await m.message.delete()
+  await force_name(c, m.message)
+
+
 ################## Callback for help button ##################
 
 @RenamerNs.on_callback_query(filters.regex('^help$'))
