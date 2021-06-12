ca9fd61c3c67bfe36ed7a75dc89ef43ffeb5bf0d
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-12 18:59:47 +0530

    Update rename.py

--- renamer/plugins/rename.py
+++ renamer/plugins/rename.py
@@ -130,7 +130,7 @@ async def media(c, m):
 
     caption = str(new_file_name)
     if Config.CUSTOM_CAPTION:
-        caption += f"\n\n {Config.CUSTOM_CAPTION}"
+        caption += f"\n\n **{Config.CUSTOM_CAPTION}**"
     as_file = (await get_data(m.from_user.id)).upload_mode
     if as_file:
         try:
