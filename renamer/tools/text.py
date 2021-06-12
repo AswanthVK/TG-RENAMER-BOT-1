389d00c9a9f3dcd9b659bc22c1184a0fce13f282
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-12 16:28:21 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -3,60 +3,53 @@ from pyrogram.emoji import *
 class TEXT:
     DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
     UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
-    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"
+    UPLOAD_SUCESS = f"{GRINNING_FACE} Thank you for using me."
     BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
     NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
-    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
-    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
-    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
-    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+    SAVED_CUSTOM_THUMBNAIL = f"{WHITE_HEAVY_CHECK_MARK} Thumbnail Saved Permanently."
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully."
+    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 T𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽. {SMILING_FACE_WITH_OPEN_MOUTH_AND_COLD_SWEAT}"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP} Your Permanent thumbnail"
 
 
-    ABOUT = """**𝖬𝗒 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 :**
+    ABOUT = """** My Name :** `Converter Bot`
+** Developer :** [AswanthVK](https://t.me/AswanthVK)
+** Credits :** `Everyone in this journey`    
+** Language :** `Python 3`
+** Library :** [Pyrogram](https://github.com/pyrogram/pyrogram)
+** Server :** [Heroku](https://herokuapp.com/)
+** Source Code :**[{BACKHAND_INDEX_POINTING_UP} Click Here](http://t.me/nokkiirunnoippokittum)
+** Build Status :** `V1.0 [BETA]`
 
-** My Name:** {bot_name}
-    
-** Language:** [Python 3](https://www.python.org/)
-
-** FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
-
-** Developer:** {bot_owner}
-
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
-
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
-
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+**Quate:** ആരും പേടിക്കണ്ട എല്ലാവർക്കും കിട്ടും പക്ഷെ എന്നെ കൊല്ലരുത്.
 """
 
     HELP_USER = """**Follow Below Steps:**
    
 ☞︎︎︎ Use /mode command to change upload mode.
 ☞︎︎︎ Send a photo to set as permanent thumbnail.
-☞︎︎︎ Now send me the Telegram file you want to rename.
-☞︎︎︎ Send the new name when bot ask.
+☞︎︎︎ Now send me the Telegram file you want to convert.
+☞︎︎︎ Send the new name for your file.
 
-For source code check about
+**Note: You must subscribe My updates Channel for using me.**
 """
 
-    START_TEXT = """Hi {user_mention},
+    START_TEXT = """Hi, **{user_mention}**
 
-I am a telegram renamer with permanent thumbnail.
+`I'm a Simple bot for Convert Telegram Media file to Streaming Video/document with Custom Thumbnail Support.`
 
 𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 𝖧𝖾𝗅𝗉.
 
-**Maintained By:** {bot_owner}
 """
 
 
     DONATE_USER = """**__Thanks for showing interest in donation.__**
  
 Donate us to keep our services continously alive
-You can send any amount 
-of 20rs, 30rs, 50rs, 70rs, 100rs, 200rs
+You can send any amount.
  
 __--Payment Methods:--__
  
 GooglePay / Paytm / PhonPay / paypal / Net Banking
  
-**For Donate:** message me @MaxxRiderz"""
+**For Donate:** message me @AswanthVK"""
