14f5127b7ac06fd89b5577ef372a3f273f65ed79
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-07-02 11:31:40 +0530

    Update short_url.py

--- renamer/plugins/short_url.py
+++ renamer/plugins/short_url.py
@@ -37,7 +37,7 @@ async def link_handler(bot, update):
         #await log_msg.reply_text(f'**User Name:** {update.from_user.mention(style="md")}\n\n**User Id:** `{update.from_user.id}`\n\n**Shortened Link :** Failed\n\nCheck logs for error')
         await update.reply(message, quote=True)
         return
-    message = f"**URL:** {url}\n\nHere is your shortlink\n`{shortened_url}`"
+    #message = f"**URL:** {url}\n\nHere is your shortlink\n`{shortened_url}`"
     markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link 🔗", url=shortened_url)]])
     # i don't think this bot with get sending message error so no need of exceptions
     #await log_msg.reply_text(text=f"**User Name :** [{update.from_user.first_name}](tg://user?id={update.from_user.id})\n\n**User Id :** `{update.from_user.id}`\n\n**Shortened Link :** {shortened_url}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
@@ -47,13 +47,9 @@ def get_shortlink(url):
     shortened_url = None
     Err = None
     try:
-       #if Config.BITLY_KEY:
-           #''' Bittly Shorten'''
-           #s = Shortener(Config.BITLY_KEY)
-           #shortened_url = s.bitly.short(url)
        if Config.BITLY_KEY:
            ''' Bittly Shorten'''
-           s = Shortener()
+           s = Shortener(Config.BITLY_KEY)
            shortened_url = s.bitly.short(url)
        else:
            ''' Da.gd : I prefer this '''
