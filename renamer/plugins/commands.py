a8444ed44cf5471e31611a82ea8d72a4e1c2e744
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-07-24 13:28:45 +0530

    Update commands.py

--- renamer/plugins/commands.py
+++ renamer/plugins/commands.py
@@ -1,11 +1,15 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import asyncio
 from ..config import Config
 from ..tools.text import TEXT
 from ..database.database import *
+from ..database.database import add_user, query_msg
+from ..database.support import users_info
 from pyrogram import Client as RenamerNs, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.errors import PeerIdInvalid, ChannelInvalid, FloodWait, UserNotParticipant
 from pyrogram.emoji import *
 
 
@@ -40,6 +44,9 @@ async def help(c, m, cb=False):
 
 @RenamerNs.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(c, m, cb=False):
+    id = m.from_user.id
+    user_name = '@' + m.from_user.username if m.from_user.username else None
+    await add_user(id, user_name)
     owner = await c.get_users(Config.OWNER_ID)
     button = [[
         InlineKeyboardButton(f'{INFORMATION} Help', callback_data="help"),
@@ -62,6 +69,46 @@ async def start(c, m, cb=False):
             quote=True
         ) 
 
+
+@RenamerNs.on_message(filters.private & filters.command('subscribers'))
+async def subscribers_count(bot, m: Message):
+    id = m.from_user.id
+    if id not in Config.AUTH_USERS:
+        return
+    msg = await m.reply_text(TEXT.WAIT_MSG)
+    messages = await users_info(bot)
+    active = messages[0]
+    blocked = messages[1]
+    await m.delete()
+    await msg.edit(TEXT.USERS_LIST.format(active, blocked))
+
+
+# ------------------------ Send messages to subs ----------------------------- #
+@RenamerNs.on_message(filters.private & filters.command('send'))
+async def send_text(bot, m: Message):
+    id = m.from_user.id
+    if id not in Config.AUTH_USERS:
+        return
+    if (" " not in m.text) and ("send" in m.text) and (m.reply_to_message is not None):
+        query = await query_msg()
+        for row in query:
+            chat_id = int(row[0])
+            try:
+                await bot.copy_message(
+                    chat_id=chat_id,
+                    from_chat_id=m.chat.id,
+                    message_id=m.reply_to_message.message_id,
+                    caption=m.caption,
+                    reply_markup=m.reply_markup
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except Exception:
+                pass
+    else:
+        msg = await m.reply_text(TEXT.REPLY_ERROR, m.message_id)
+        await asyncio.sleep(8)
+        await msg.delete()
 
 ################## about command ##################
 
