2aad07c969047d69abdf4526dd82805debbc169c
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-07-24 15:31:19 +0530

    Update commands.py

--- renamer/plugins/commands.py
+++ renamer/plugins/commands.py
@@ -5,8 +5,6 @@ import asyncio
 from ..config import Config
 from ..tools.text import TEXT
 from ..database.database import *
-#from renamer.database.database import add_user, query_msg
-from ..database.support import *
 from pyrogram import Client as RenamerNs, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pyrogram.errors import PeerIdInvalid, ChannelInvalid, FloodWait, UserNotParticipant
@@ -44,9 +42,6 @@ async def help(c, m, cb=False):
 
 @RenamerNs.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(c, m, cb=False):
-    #id = m.from_user.id
-    #user_name = '@' + m.from_user.username if m.from_user.username else None
-    #await add_user(id, user_name)
     owner = await c.get_users(Config.OWNER_ID)
     owner_username = owner.username if owner.username else 'Ns_bot_updates'
     button = [[
